e9d6facaff16e034a5f5ed61bcf999635aaf8d4e
Ian Solorzano <ian.cyber@outlook.com>
2026-03-10 22:32:55 -0500

    Create README.d

---
+++ soc-playbook/README.d
@@ -0,0 +1,4 @@
+This repository contains SOC investigation playbooks and detection methodologies 
+based on general blue team learning and practice labs.
+
+All examples are sanitized and recreated for educational purposes.
